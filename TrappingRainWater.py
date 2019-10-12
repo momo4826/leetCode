79c21772d9565954f825163dc5c5a2cdbbf5315c
Echo <furong_g@163.com>
2019-10-12 16:28:41 +0200

    Create TrappingRainWater.py

---
+++ TrappingRainWater.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height or len(height) < 3:
+            return 0
+        waterLevel = []
+        left = 0
+        for h in height:
+            left = max(left, h)
+            waterLevel += [left]
+        right = 0
+        for i, h in reversed(list(enumerate(height))):
+            right = max(right, h)
+            waterLevel[i] = min(waterLevel[i], right) - h
+        return sum(waterLevel)
