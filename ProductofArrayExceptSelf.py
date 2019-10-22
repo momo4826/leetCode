1dea34ece4e00ddd3dba132e2059ee7a1ff1e7be
Echo <furong_g@163.com>
2019-10-22 15:07:42 +0200

    Update ProductofArrayExceptSelf.py

--- ProductofArrayExceptSelf.py
+++ ProductofArrayExceptSelf.py
@@ -1,3 +1,31 @@
+#me
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        prod = 1
+        count = 0
+        for num in nums:
+            if num != 0:
+                prod = prod * num
+            else:
+                count += 1
+        res = []
+        if count == 0:
+            for num in nums:
+                res += [prod / num]
+        elif count == 1:
+            for num in nums:
+                if num == 0:
+                    res += [prod]
+                else:
+                    res += [0]
+        else:
+            res = [0] * len(nums)
+        return res
+#Left and Right product lists
 class Solution:
     def productExceptSelf(self, nums):
         """
@@ -17,3 +45,20 @@ class Solution:
         for i in range(length):
             answer[i] = L[i] * R[i]        
         return answer
+#O(1) space approach
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = [0] * len(nums)
+        answer[0] = 1
+        for i in range(1, len(nums)):
+            answer[i] = answer[i - 1] * nums[i - 1]
+        R = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] = answer[i] * R
+            R= R * nums[i]
+        return answer
+    
