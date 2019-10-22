ec8bc6ead67f4980c0bcd5466c697fae7f705c2e
Echo <furong_g@163.com>
2019-10-22 21:51:00 +0200

    Update and rename MergekSortedLists.py to MergeKSortedLists.py

--- MergeKSortedLists.py
+++ MergeKSortedLists.py
@@ -61,3 +61,24 @@ class Solution(object):
             res = res.next
         res.next = l or r
         return answer.next
+#priority queue
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
